6c93abf68579c3e663ab3ef7bb8558d7e009b4b4
Louis GALLET <46707587+louisgallet95@users.noreply.github.com>
2020-10-30 18:13:00 +0100

    First website beta

--- views/website/js/script.js
+++ views/website/js/script.js
@@ -3,6 +3,21 @@ const axios = require("axios");
 function fetchData() {
   axios.get("https://api.covid19api.com/summary").then((res) => {
     console.log(res.data.Global);
+    const newConfirmed = res.data.Global.NewConfirmed;
+    const newDeaths = res.data.Global.NewDeaths;
+    const newRecovered = res.data.Global.NewRecovered;
+    document.getElementById("totalCase").innerHTML =
+      "Nous avons " +
+      newConfirmed +
+      " nouveaux cas dans le monde par rapport à hier";
+    document.getElementById("newDeaths").innerHTML =
+      "Nous avons " +
+      newDeaths +
+      " nouveaux décès dans le monde par rapport à hier";
+    document.getElementById("newRecovered").innerHTML =
+      "Nous avons " +
+      newRecovered +
+      " nouvelles personnes sauvées dans le monde par rapport à hier";
   });
 }
 
