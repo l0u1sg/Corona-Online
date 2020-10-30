31b0820d18a7e096abe642f69ccc6b2fc45ad448
Louis GALLET <46707587+louisgallet95@users.noreply.github.com>
2020-10-30 16:57:49 +0100

    add API

--- views/website/js/script.js
+++ views/website/js/script.js
@@ -1,9 +1,9 @@
-var requirejs = require("requirejs");
+const axios = require("axios");
 
-// function fetchData() {
-//   axios.get("https://api.covid19api.com/summary").then((res) => {
-//     console.log(res.data);
-//   });
-// }
+function fetchData() {
+  axios.get("https://api.covid19api.com/summary").then((res) => {
+    console.log(res.data.Global);
+  });
+}
 
-// fetchData();
+fetchData();
