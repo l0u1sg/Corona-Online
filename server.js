9d624f05c6d8087d9e3b84ad9ed07e664f533301
Louis GALLET <46707587+louisgallet95@users.noreply.github.com>
2020-11-06 09:11:40 +0100

    simplyfi + desactive maintenance mode

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ const express = require("express");
 const app = express();
 const _httpErrorPages = require("http-error-pages");
 const port = process.env.PORT || 3000;
+const maintenance = false
 
 app.set("view-engine", "ejs");
 app.use(express.static(__dirname + "/views"));
 
-app.get("/", (req, res) => {
-  res.render("maintenance/index.ejs");
-});
-app.get("/v3", (req, res) => {
-  res.render("website/index.ejs");
-});
-
+if (maintenance == true) {
+  app.get("/", (req, res) => {
+    res.render("maintenance/index.ejs")
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.render("website/index.ejs")
+  })
+}
 
 _httpErrorPages.express(app, {
   lang: "fr_FR",
